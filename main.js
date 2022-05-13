59670d61e599049b1d9e3e13073a052e23b6f380
rmrose78 <ryan@Ryans-Air.attlocal.net>
2022-05-13 10:02:12 -0400

    Updates Made from review. Refactored functions into Dice Object, added Prettier for auto formating

--- main.js
+++ main.js
@@ -1,99 +1,119 @@
-// --- DOM Elements --- \\
-const valuePlayer_1 = document.getElementById("player1-score--value");
-const valuePlayer_2 = document.getElementById("player2-score--value");
+// --- DOM ELEMENTS --- \\
+
+const activePlayer1 = document.getElementById("player1");
+const activePlayer2 = document.getElementById("player2");
+const valuePlayer1 = document.getElementById("player1-score--value");
+const valuePlayer2 = document.getElementById("player2-score--value");
 const dicePositions = document.getElementsByClassName("dice");
 const uncommitedPoints = document.getElementById("currentPoints");
 
-// --- Roll Dice --- \\
+// --- ROLL DICE --- \\
 
-// Dice Values array and boolean array on wether a dice at that position should be rolled.
 const dice = {
-    positionValues: [0, 0, 0, 0, 0, 0],
-    rollBoolean: [true, true, true, true, true, true],
-};
+  positionValues: [0, 0, 0, 0, 0, 0],
+  shouldRoll: [true, true, true, true, true, true],
+  positions: [0, 1, 2, 3, 4, 5],
+  images: [
+    "./images/blank_dice.png",
+    "./images/1_dice.png",
+    "./images/2_dice.png",
+    "./images/3_dice.png",
+    "./images/4_dice.png",
+    "./images/5_dice.png",
+    "./images/6_dice.png",
+  ],
 
-// Roll Random number between 1-6.
-const rollDie = () => Math.floor(Math.random() * 6) + 1;
+  /**
+   * Roll a random number between 1-6.
+   */
+  rollDie: function () {
+    return Math.floor(Math.random() * 6) + 1;
+  },
 
-// Roll each die based on the Boolean array value for that die.
-const rollAvailable = () => {
-    // dice selected prior to new roll will have rollBoolean as false.
-    lockSelectedDiceOnRoll()
-
-    // Roll Dice if position in boolean array is true
-    for (let i = 0; i < dice.positionValues.length; i++) {
-        if (dice.rollBoolean[i]) dice.positionValues[i] = rollDie();
+  /**
+   * Roll each die based if shouldRoll array is true for that die.
+   */
+  rollAvailable: function () {
+    for (const die of this.positions) {
+      if (this.shouldRoll[die]) this.positionValues[die] = this.rollDie();
     }
-};
+  },
 
-// Display dice - if 0 show blank die else show die image of the value.
-const displayDice = () => {
-    for (const die in dice.positionValues) {
-    dice.positionValues[die] === 0
-    ? (dicePositions[die].src = `./images/blank_dice.png`)
-    : (dicePositions[die].src = `./images/${dice.positionValues[die]}_dice.png`);
-}
-};
-
-// dice selected will change rollBoolean to false to prevent rerolling that die.
-const lockSelectedDiceOnRoll = () => {
-    for (let i = 0; i < dicePositions.length; i++) {
-        if (dicePositions[i].classList[1] === "select") dice.rollBoolean[i] = false
+  /**
+   * Display dice - If 0 show blank die else show die image of the value.
+   */
+  display: function () {
+    for (const die of this.positions) {
+      this.positionValues[die] === 0
+        ? (dicePositions[die].src = this.images[0])
+        : (dicePositions[die].src = this.images[this.positionValues[die]]);
     }
-    console.log(`boolean ${dice.rollBoolean}`)
-}
+  },
 
-// --- Select --- \\
+  /**
+   * Dice selected will update shouldRoll to false to prevent rerolling that die.
+   */
+  updateShouldRoll: function () {
+    for (const die of this.positions) {
+      if (dicePositions[die].classList[1] === "select")
+        this.shouldRoll[die] = false;
+    }
+  },
 
-// Add event listener on each die.
-for (let i = 0; i < dicePositions.length; i++) {
-        dicePositions[i].addEventListener("click", () => {
-            // toggle Select as long as die is not blank or have "lock" as a class.
-            if (dice.positionValues[0] === 0) {
-                return
-            } else if (dice.rollBoolean[i]) {
-                dicePositions[i].classList.toggle("select");
-            }
+  /**
+   * Remove 'select' class from all dice
+   */
+  removeAllSelect: function () {
+    dicePositions.forEach((die) => {
+      dicePositions[die].classList.toggle("select");
     });
+  },
+};
+
+// --- SELECT --- \\
+
+// Event listers added on all dice for toggling the 'select' class
+for (const die of dice.positions) {
+  dicePositions[die].addEventListener("click", () => {
+    dicePositions[die].classList.toggle("select");
+  });
 }
 
-// --- Hold --- \\
+// --- HOLD --- \\
 
 // Hold Dice - Switch player
-document.getElementById("hold-button").addEventListener('click',() => {
-    const activePlayer_1 = document.getElementById("player1")
-    const activePlayer_2 = document.getElementById("player2")
+document.getElementById("hold-button").addEventListener("click", () => {
+  activePlayer1.classList.toggle("active");
+  activePlayer2.classList.toggle("active");
 
-    //switch Player
-    activePlayer_1.classList.toggle("active")
-    activePlayer_2.classList.toggle("active")
+  points.roundTotal = 0;
+  points.selectedTotal = 0;
 
-    //reset on roundTotal and selectedTotal points
-    points.roundTotal = 0
-    points.selectedTotal = 0
+  dice.removeAllSelect();
+  dice.positionValues = [0, 0, 0, 0, 0, 0];
+  dice.shouldRoll = [true, true, true, true, true, true];
+  dice.display();
+});
 
-})
+// --- SCORE --- \\
 
-// --- Score --- \\
-
-// Points information
 points = {
-    player1: 0,
-    player2: 0,
-    roundTotal: 0,
-    selectedTotal: 0
-}
-
+  player1: 0,
+  player2: 0,
+  roundTotal: 0,
+  selectedTotal: 0,
+};
 
 // positionValues Dice rolled lead to points - Reset Roll
 
-// Reset Game
+// --- RESET --- \\
 
-// --- Event Listeners --- \\
+// --- EVENT LISTENERS --- \\
 document.getElementById("roll-button").addEventListener("click", () => {
-    rollAvailable();
-    displayDice();
-    console.log(dice.positionValues);
+  dice.updateShouldRoll();
+  dice.rollAvailable();
+  dice.display();
+  console.log(dice.positionValues);
 });
 
 // dicePositions.forEach(die => {
